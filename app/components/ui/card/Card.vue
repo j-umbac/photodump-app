1c7232a646185e107fa6c942c0d70b055e833277
Joshua Umbac <joshuarheyumbac14@gmail.com>
2026-06-16 20:28:42 +0800

    fix: minimize hover scaling

--- app/components/ui/card/Card.vue
+++ app/components/ui/card/Card.vue
@@ -14,7 +14,7 @@ const props = withDefaults(defineProps<{
   <div
     data-slot="card"
     :data-size="size"
-    :class="cn('bg-white text-card-foreground gap-6 overflow-hidden rounded-2xl py-6 text-sm shadow-[0_4px_24px_rgba(0,0,0,0.04)] border border-zinc-100/50 has-[>img:first-child]:pt-0 data-[size=sm]:gap-4 data-[size=sm]:py-4 *:[img:first-child]:rounded-t-2xl *:[img:last-child]:rounded-b-2xl group/card flex flex-col transition-all duration-300 hover:scale-[1.02] ease-apple-ease', props.class)"
+    :class="cn('bg-white text-card-foreground gap-6 overflow-hidden rounded-2xl py-6 text-sm shadow-[0_4px_24px_rgba(0,0,0,0.04)] border border-zinc-100/50 has-[>img:first-child]:pt-0 data-[size=sm]:gap-4 data-[size=sm]:py-4 *:[img:first-child]:rounded-t-2xl *:[img:last-child]:rounded-b-2xl group/card flex flex-col transition-all duration-300 hover:scale-[1.01] ease-apple-ease', props.class)"
   >
     <slot />
   </div>
