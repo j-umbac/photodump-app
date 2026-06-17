ee0938284ce22460e6b08165d48bde5322bc611f
Joshua Umbac <joshuarheyumbac14@gmail.com>
2026-06-17 10:46:38 +0800

    feat: create footer

---
+++ app/components/Footer.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <footer class="w-full border-t bg-background/50 backdrop-blur">
+        <div class="container max-w-6xl mx-auto px-6 h-16 flex items-center justify-between text-sm text-muted-foreground">
+            <p>
+                Created by <a href="https://github.com/j-umbac" target="_blank" class="font-medium hover:text-primary transition-colors hover:underline">Joshua</a>
+            </p>
+            <div class="flex items-center space-x-4">
+                <a href="https://github.com/j-umbac/photodump-app" target="_blank" class="font-medium hover:text-primary transition-colors hover:underline flex items-center gap-1">
+                    <Icon name="mdi:github" class="text-xl"/>
+                    <span>
+                        GitHub
+                    </span>
+                </a>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
